cf2a83728e9021801414d65066f4a4bd1f5bef95
ivanpopovic <i.popovic@gmx.at>
2016-12-05 16:53:03 +0100

    feat: set onclick marker with info

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -1,15 +1,29 @@
 var map;
+
 function initMap() {
+  var pos;
+  var map;
+  var markers = [];
+  var marker_infobox = "test";
+
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
     zoom: 16
   });
-  var infoWindow = new google.maps.InfoWindow({map: map});
+  var infoWindow = new google.maps.InfoWindow({
+    map: map
+  });
+  var infowindow = new google.maps.InfoWindow({
+    content: marker_infobox
+  });
 
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
+      pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
@@ -24,11 +38,32 @@ function initMap() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
+
+  function addMarker(location) {
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      animation: google.maps.Animation.DROP
+    });
+    markers.push(marker);
+    markers[markers.length - 1].addListener('click', function(event) {
+      infowindow.open(map, marker);
+    });
+
+  }
+
+  map.addListener('click', function(event) {
+    addMarker(event.latLng);
+  });
+
+
+
+
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
 }
